dc758deb92b10709feda0e62bfd02b03aee99051
vineboneto <vineboneto@gmail.com>
2020-11-06 09:52:54 -0300

    inital commit

---
+++ main.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct sNodo {
+    struct sNodo *left;
+    struct sNodo *right;
+    int data;
+} Nodo;
+
+Nodo* createNodo();
+void insert(Nodo*, int);
+int delete (Nodo*, int);
+Nodo* search(Nodo*, int);
+
+int getMin(Nodo*);
+
+int main () {
+
+    Nodo* root = createNodo();
+    root->data = 5;
+
+
+    insert(root, 2);
+    insert(root, 12);
+    insert(root, 7);
+    insert(root, 6);
+    insert(root, 2);
+    insert(root, 3);
+    insert(root, 4);
+    insert(root, 1);
+
+
+    printf("Root: %d\n", root->data);
+    printf("Root-right: %d\n", root->right->data);
+    printf("Root-left: %d\n", root->left->data);
+    printf("Root-right-left: %d\n", root->right->left->data);
+    printf("Root-left-right: %d\n", root->left->right->data);
+
+
+    // delete(root, 2);
+
+    // Nodo* res = search(root, 2);
+    // if (res) {
+    //     printf("Found number: %d\n", res->data);
+    // } else {
+    //     printf("Not Found Number");
+    // }
+
+
+    root = NULL;
+    free(root);
+    return 0;
+}
+
+Nodo* createNodo() {
+    Nodo* nd = NULL;
+    nd = (Nodo*) malloc(sizeof(Nodo));
+    nd->left = NULL;
+    nd->right = NULL;
+    if (!nd) {
+        exit(EXIT_FAILURE);
+        perror("Overflow!!!");
+    }
+    return nd;
+}
+
+void insert(Nodo* root, int data) {
+    Nodo* nodo;
+
+
+    // if (!root) {
+    //     root = createNodo();
+    //     root->data = data;
+    
+    // } else {
+    //     if (data > root->right) insert(root->right, data);
+
+    //     if (data < root->left) insert(root->left, data);
+    // }
+
+    if (data > root->data) {
+        if (root->right) { 
+            insert(root->right, data);
+        } else {
+            nodo = createNodo();
+            nodo->data = data;
+            root->right = nodo;
+        }
+    } else {
+        if (root->left) {
+            insert(root->left, data);
+        } else {
+            nodo = createNodo();
+            nodo->data = data;
+            root->left = nodo;
+        }
+    }
+}
+
+int delete(Nodo* root, int data) {
+
+    Nodo* aux;
+    Nodo* nodo = search(root, data);
+    if (nodo == NULL) { // Elemento existe ?
+        return -1;
+    }
+
+    // Primeiro caso não possui filhos
+    // aux = nodo;
+    // if (nodo->right && !nodo->left) {          //Possui somente um filho
+    //     // nodo->right->left = nodo->left;                       
+    // } else if (nodo->left && !nodo->right) {   //Possui somente um filho
+    //     nodo = nodo->left;
+    // } else if (nodo->right && nodo->left) {    //Possui os dois filhos
+    //     Nodo* min = getMin(nodo->right);
+    //     nodo = min;
+    // }
+
+    printf("Number %d deleted\n", nodo->data);
+    nodo = NULL;
+    free(nodo);
+    return 1;
+}
+
+// Pegar o menor valor do ramo direitos
+int getMin(Nodo* root) {
+    int min;
+    
+    if (!root->left) {
+        min = root->data;
+    } else {
+        min = getMin(root->left);
+    }
+
+    return min;
+}
+
+Nodo* search(Nodo* root, int data) {
+    if (root->data != data && !root->left && !root->right) {
+        return NULL;
+    }
+
+    if (root->data == data) {
+        return root;
+    } else {
+        data > root->data ? search(root->right, data) : search(root->left, data);
+    }
+}
